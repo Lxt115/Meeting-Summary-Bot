b84b947a5b77a29e8b6e8fdb006a7987004b57dd
Lxt115 <67227722+Lxt115@users.noreply.github.com>
2024-01-18 12:46:02 +0800

    Update sum_model.py

--- models/sum_model.py
+++ models/sum_model.py
@@ -6,20 +6,35 @@ from langchain.docstore.document import Document
 from langchain.chains import MapReduceDocumentsChain, ReduceDocumentsChain
 
 
-def summarize(script):
-    # file_path = "../test.txt"
-    #
-    # with open(file_path, "r", encoding="utf-8") as file:
-    #     script = file.read()
+class Sum():
+    def __init__(self, args):
+        self.llm_version = args.llm_version
+        # self.max_tokens = args.qa_max_new_tokens
 
-    text_splitter = CharacterTextSplitter(chunk_size=10, separator="\n", chunk_overlap=0)
-    texts = text_splitter.split_text(script)
+    def summarize(self, script):
+        text_splitter = CharacterTextSplitter(chunk_size=10, separator="\n", chunk_overlap=0)
+        texts = text_splitter.split_text(script)
 
-    docs = [Document(page_content=t) for t in texts]
+        docs = [Document(page_content=t) for t in texts]
 
-    llm = TransformersLLM.from_model_id_low_bit(f"D:\\Mcs\\5014\\llm\\models\\Llama-2-7b-chat-hf-INT4")
-    # chain = load_summarize_chain(llm, chain_type="map_reduce", token_max=256)
-    chain = load_summarize_chain(llm, chain_type="refine")
-    # result = chain.run(docs)
-    result = "pass"
-    return result
+        llm = TransformersLLM.from_model_id_low_bit(f"D:\\Mcs\\5014\\VChat-BigDL\\checkpoint\\{self.llm_version}")
+        # chain = load_summarize_chain(llm, chain_type="map_reduce", token_max=256)
+        chain = load_summarize_chain(llm, chain_type="refine")
+        result = chain.run(docs)
+        result = "pass"
+        return result
+
+
+# for test
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--llm_version", default="Llama-2-7b-chat-hf-INT4", help="LLM model version")
+args = parser.parse_args()
+
+file_path = "../test.txt"
+with open(file_path, "r", encoding="utf-8") as file:
+    content = file.read()
+
+Sumbot = Sum(args)
+Sumbot.summarize(content)
