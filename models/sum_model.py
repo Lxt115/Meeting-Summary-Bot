e935b12e659de5f5cf1605f0f43907e75dfa26cf
Lxt115 <67227722+Lxt115@users.noreply.github.com>
2024-01-20 22:06:31 +0800

    Update sum_model.py

--- models/sum_model.py
+++ models/sum_model.py
@@ -1,9 +1,5 @@
 from bigdl.llm.langchain.llms import TransformersLLM
-from langchain.prompts import PromptTemplate
-from langchain.chains.summarize import load_summarize_chain
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.docstore.document import Document
-from langchain.chains import MapReduceDocumentsChain, ReduceDocumentsChain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
 class Sum():
@@ -12,29 +8,50 @@ class Sum():
         # self.max_tokens = args.qa_max_new_tokens
 
     def summarize(self, script):
-        text_splitter = CharacterTextSplitter(chunk_size=10, separator="\n", chunk_overlap=0)
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1300, chunk_overlap=0)
         texts = text_splitter.split_text(script)
 
-        docs = [Document(page_content=t) for t in texts]
+        prompt_template = """The following is a piece of meeting recording:
+        <<<{text}>>>
+        Based on recording, summary the main idea fluently. 
+        JUST SUMMARY!NO OTHER WORDS!
+        SUMMARY:"""
+
+        reduce_template = """The following is a meeting recording pieces:
+        <<<{text}>>>
+        Take these and distill it into a final, consolidated summary of the meeting. 
+        JUST SUMMARY!NO OTHER WORDS!
+        SUMMARY:"""
+
+        print(len(texts))
 
         llm = TransformersLLM.from_model_id_low_bit(f"D:\\Mcs\\5014\\VChat-BigDL\\checkpoint\\{self.llm_version}")
-        # chain = load_summarize_chain(llm, chain_type="map_reduce", token_max=256)
-        chain = load_summarize_chain(llm, chain_type="refine")
-        result = chain.run(docs)
-        result = "pass"
-        return result
+        sum_split = []
+
+        for text in texts:
+            response = llm(prompt=prompt_template.format(text=text), max_new_tokens=1000)
+            print(response)
+            response_answer = response.split("SUMMARY:")
+
+            sum_split.append(response_answer[1])
+
+        sum_all = "\n".join(sum_split)
+
+        result = llm(prompt=reduce_template.format(text=sum_all),  max_new_tokens=4000)
+        result_split = result.split("SUMMARY:")
+        return result_split[1]
 
 
 # for test
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--llm_version", default="Llama-2-7b-chat-hf-INT4", help="LLM model version")
-args = parser.parse_args()
-
-file_path = "../test.txt"
-with open(file_path, "r", encoding="utf-8") as file:
-    content = file.read()
-
-Sumbot = Sum(args)
-Sumbot.summarize(content)
+# import argparse
+#
+# parser = argparse.ArgumentParser()
+# parser.add_argument("--llm_version", default="Llama-2-7b-chat-hf-INT4", help="LLM model version")
+# args = parser.parse_args()
+# file_path = "../test.txt"
+# with open(file_path, "r", encoding="utf-8") as file:
+#     content = file.read()
+# Sumbot = Sum(args)
+# result = Sumbot.summarize(content)
+#
+# print(result)
