7ada3ebd1b8fef1119ccb1786c53f8a0331384a5
Lxt115 <67227722+Lxt115@users.noreply.github.com>
2024-01-25 00:15:53 +0800

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,24 +1,5 @@
-import signal
 import os
 import contextlib
-
-def format_time(seconds):
-    hours = seconds // 3600
-    minutes = (seconds // 60) % 60
-    seconds = seconds % 60
-    return f"{hours}:{minutes:02d}:{seconds:02d}"
-
-class timeout:
-    def __init__(self, seconds=1, error_message='Timeout'):
-        self.seconds = seconds
-        self.error_message = error_message
-    def handle_timeout(self, signum, frame):
-        raise TimeoutError(self.error_message)
-    def __enter__(self):
-        signal.signal(signal.SIGALRM, self.handle_timeout)
-        signal.alarm(self.seconds)
-    def __exit__(self, type, value, traceback):
-        signal.alarm(0)
         
 @contextlib.contextmanager
 def new_cd(x):
